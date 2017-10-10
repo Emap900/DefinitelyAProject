472e7fd919a68d1afb3bb09986e07464addad1d2
Carl-Tang <ytan415@aucklanduni.ac.nz>
2017-10-10 18:16:01 +1300

    PersonalPane Layout

--- src/application/PersonalPanelController.java
+++ src/application/PersonalPanelController.java
@@ -1,17 +1,57 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.control.Label;
 
 public class PersonalPanelController {
 
+	@FXML
+	private Label _name;
+
+	@FXML
+	private Label _latestScore;
+
+	@FXML
+	private Label _latestMode;
+
+	@FXML
+	private Label _normalModeRank;
+
+	@FXML
+	private Label _normalModeScore;
+
+	@FXML
+	private AreaChart<?, ?> _normalModeChart;
+
+	@FXML
+	private Label _endlessModeRank;
+
+	@FXML
+	private Label _endlessModeScore;
+
+	@FXML
+	private AreaChart<?, ?> _endlessModeChart;
+
+	@FXML
+	void backToHome(ActionEvent event) {
+		// TODO Auto-generated method stub
+	}
+
+	@FXML
+	void showLeaderBoard(ActionEvent event) {
+		// TODO Auto-generated method stub
+	}
+
 	public void setParent(FoundationBoardController foundationBoardController) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void showPersonalHistory(String _playerName) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
