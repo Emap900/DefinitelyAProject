befa718be9da5afbd20cdafde9490640f99f3911
Carl-Tang <ytan415@aucklanduni.ac.nz>
2017-10-16 13:33:11 +1300

    Controller for add new question dialog

---
+++ src/application/AddNewQuestionDialogController.java
@@ -0,0 +1,25 @@
+package application;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AddNewQuestionDialogController {
+
+    @FXML
+    private JFXTextField questionTextField;
+
+    @FXML
+    private JFXTextField answerTestField;
+
+    @FXML
+    private JFXButton confirmAdd;
+
+    @FXML
+    void addQuestion(ActionEvent event) {
+
+    }
+
+}
