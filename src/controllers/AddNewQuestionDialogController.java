27afda708c9c1c9a7b6e75b210702a5c598a5325
Carl-Tang <ytan415@aucklanduni.ac.nz>
2017-10-21 18:56:45 +1300

    Modified AddNewQuestionDialog
    Modifies layout.
    Implemented auto calculate feature

--- src/controllers/AddNewQuestionDialogController.java
+++ src/controllers/AddNewQuestionDialogController.java
@@ -3,12 +3,20 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
+import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import models.QuestionModel;
 
@@ -16,18 +24,23 @@ public class AddNewQuestionDialogController implements Initializable {
 
 	@FXML
 	private JFXTextField questionTextField;
-
 	@FXML
-	private JFXTextField answerTestField;
-
+	private JFXTextField answerTextField;
+	@FXML
+	private Label answerFormatWarningLabel;
 	@FXML
 	private JFXButton confirmAdd;
+	@FXML
+	private VBox warningBox;
 
 	private String _setName;
+
+	private boolean _answerIsValid;
 
 	private QuestionSetEditPanelController _parent;
 
 	private Stage _stage;
+	private boolean _questionIsValid;
 
 	public void setParent(QuestionSetEditPanelController parent) {
 		_parent = parent;
@@ -37,10 +50,10 @@ public class AddNewQuestionDialogController implements Initializable {
 	void addQuestion(ActionEvent event) {
 		// TODO add multiple questions bbb
 		String question = questionTextField.getText();
-		String answer = answerTestField.getText();
+		String answer = answerTextField.getText();
 		QuestionModel.getInstance().addQuestionToQuestionSet(_setName, question, answer);
 		_parent.loadQuestions();
-		_stage.close();
+		// _stage.close();
 	}
 
 	public void initData(String setName, Stage newQuestionStage) {
@@ -50,19 +63,42 @@ public class AddNewQuestionDialogController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		answerFormatWarningLabel
+				.setText("Answer must be a number between " + Main.NUMLOWERBOUND + " and " + Main.NUMUPPERBOUND);
+
 		questionTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+
 			if (newValue.isEmpty()) {
 				confirmAdd.setDisable(true);
 			} else {
-				confirmAdd.setDisable(false);
+				ScriptEngineManager mgr = new ScriptEngineManager();
+				ScriptEngine engine = mgr.getEngineByName("JavaScript");
+				try {
+					int answer = Integer.parseInt(engine.eval(newValue).toString());
+					if (answer >= Main.NUMLOWERBOUND && answer <= Main.NUMUPPERBOUND) {
+						answerTextField.setText(answer + "");
+					} else {
+						answerTextField.setText("");
+						warningBox.setVisible(true);
+						confirmAdd.setDisable(true);
+					}
+				} catch (ScriptException | NumberFormatException e) {
+					answerTextField.setText("");
+					warningBox.setVisible(true);
+					confirmAdd.setDisable(true);
+				}
 			}
 		});
 
-		answerTestField.textProperty().addListener((observable, oldValue, newValue) -> {
-			if (newValue.isEmpty()) {
-				confirmAdd.setDisable(true);
-			} else {
+		answerTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+
+			// check is the answer empty
+			if (!newValue.isEmpty()) {
+				warningBox.setVisible(false);
 				confirmAdd.setDisable(false);
+			} else {
+				warningBox.setVisible(true);
+				confirmAdd.setDisable(true);
 			}
 		});
 	}
