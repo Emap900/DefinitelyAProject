a04771dc8992de02b6cfccc08b2e229d1fbce1e5
Emap900 <31753795+Emap900@users.noreply.github.com>
2017-10-22 17:26:36 +1300

    fix default list not show

--- src/models/QuestionModel.java
+++ src/models/QuestionModel.java
@@ -318,9 +318,9 @@ public class QuestionModel {
 
 	// getListOfQuestions in a specific set
 	public List<List<String>> getQuestionsFromSpecificSet(String setName) {
-	
-		System.out.println("Step 3 succeed.");
-	
+		if(setName.equals("Default")) {
+				return _preloadSortedQuestionSet;
+		}
 		return _sets.get(setName).getQuestionsInSet();
 	}
 
